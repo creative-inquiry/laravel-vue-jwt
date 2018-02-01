2e107e51be26ec988ae87f45e08a27123b0a0ec1
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-01 12:06:52 +1100

    Setup jwt-auth library as per documentation

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api', // updated
         'passwords' => 'users',
     ],
 
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt', // updated
             'provider' => 'users',
         ],
     ],
