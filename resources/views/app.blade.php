d0abe0c12e4052b40d97a4034d9471b7d84106ae
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-04 17:16:05 +1100

    Use <router-view> in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
 </head>
 <body>
     <div id="app">
-        <example-component></example-component>
+        <router-view></router-view>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
