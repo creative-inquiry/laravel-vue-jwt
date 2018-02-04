8a1dcb20039f48e44bfa5f3cfd25c5ed9abecc9b
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-04 17:17:06 +1100

    Create home component and some modifications to example component

--- resources/assets/js/components/HomeComponent.vue
+++ resources/assets/js/components/HomeComponent.vue
@@ -3,13 +3,12 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Home</div>
 
                     <div class="panel-body">
-                        I'm an example component 123 !
+                        I'm the home component
                     </div>
                 </div>
-                {{ response }}
             </div>
         </div>
     </div>
@@ -18,12 +17,11 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.');
-            axios.get('/', { headers: { Authorization: `Bearer test` }}).then(console.log('done'));
+
         },
         data() {
             return {
-                response: 'test'
+
             }
         }
     }
