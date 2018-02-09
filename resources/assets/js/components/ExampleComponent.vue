b7b4695164d17790ca0ef6b467ca9bddadf0a36a
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-09 17:09:35 +1100

    Remove container div (it will be in the parent) and cleanup wording

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component 123 !
-                    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Example Component</div>
+                <div class="panel-body">
+                    I'm an example component!
                 </div>
             </div>
         </div>
