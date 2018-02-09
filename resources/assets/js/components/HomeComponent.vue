abe55b846af3b5eb385a9ac104eb71c583885ada
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-09 17:11:32 +1100

    Remove container div as it will be in the parent

--- resources/assets/js/components/HomeComponent.vue
+++ resources/assets/js/components/HomeComponent.vue
@@ -1,13 +1,11 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Home</div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Home</div>
 
-                    <div class="panel-body">
-                        I'm the home component
-                    </div>
+                <div class="panel-body">
+                    I'm the home component
                 </div>
             </div>
         </div>
