0b3bbec1ea8f9a3c8c6c1176a9d2227f43ebf324
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-12 16:49:14 +1100

    Create basic login page, compile JS

---
+++ resources/assets/js/components/LoginComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login-form mx-auto text-center align-middle">
+        <form v-on:submit.prevent="submitLogin">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus v-model="email">
+
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            submitLogin() {
+                console.log(this.email);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login-form {
+        width: 300px;
+    }
+</style>
