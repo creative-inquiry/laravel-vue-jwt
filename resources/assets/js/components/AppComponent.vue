3b4335620d2bff2addc520ca25e2f07ba6c4600e
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-15 13:31:25 +1100

    Move navigation to DashboardComponent

--- resources/assets/js/components/AppComponent.vue
+++ resources/assets/js/components/AppComponent.vue
@@ -1,18 +1,5 @@
 <template>
-    <div class="container">
-        <h1>Laravel and Vue JS JWT Auth</h1>
-
-        <p>
-            <router-link :to="{ name: 'home' }">Home</router-link> |
-            <router-link :to="{ name: 'dashboard' }">Dashboard</router-link> |
-            <router-link :to="{ name: 'login' }">Login</router-link> |
-            <router-link :to="{ name: 'logout' }">Logout</router-link>
-        </p>
-
-        <div>
-            <router-view></router-view>
-        </div>
-    </div>
+    <router-view></router-view>
 </template>
 
 <script>
--- resources/assets/js/components/DashboardComponent.vue
+++ resources/assets/js/components/DashboardComponent.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <h1>Laravel and Vue JS JWT Auth</h1>
+
+            <p>
+                <router-link :to="{ name: 'home' }">Home</router-link> |
+                <router-link :to="{ name: 'dashboard' }">Dashboard</router-link> |
+                <router-link :to="{ name: 'login' }">Login</router-link> |
+                <router-link :to="{ name: 'logout' }">Logout</router-link>
+            </p>
+
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
                 <div class="panel-body">
