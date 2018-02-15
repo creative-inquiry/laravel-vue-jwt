339279597ca6f54a769ef184479c53b4ed73e119
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-15 12:49:24 +1100

    On load, check local storage for token to determine if user is logged in

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        isLoggedIn: false
+        isLoggedIn: !!localStorage.getItem('token')
     },
     mutations: {
         loginUser (state) {
