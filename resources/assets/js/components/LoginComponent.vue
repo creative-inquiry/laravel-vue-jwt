347d037cfcfff7d6abe72ee0448132406ad06064
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-19 13:12:00 +1100

    Complete the submitLogin() method, add styling

--- resources/assets/js/components/LoginComponent.vue
+++ resources/assets/js/components/LoginComponent.vue
@@ -1,42 +1,82 @@
 <template>
-    <div class="login-form mx-auto text-center align-middle">
-        <form v-on:submit.prevent="submitLogin">
+    <div class="text-center form-wrapper">
+
+        <form class="form-signin" v-on:submit.prevent="submitLogin">
+            <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 
             <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus v-model="email">
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus v-model="email">
 
             <label for="inputPassword" class="sr-only">Password</label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
 
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
+
     </div>
 </template>
 
 <script>
+    import store from '../store'
     export default {
         data() {
             return {
                 email: '',
-                password: ''
+                password: '',
+                loginError: false,
             }
         },
         methods: {
             submitLogin() {
-                console.log(this.email);
+                this.loginError = false;
+                axios.post('/api/auth/login', {
+                    email: this.email,
+                    password: this.password
+                }).then(response => {
+                    // login user, store the token and redirect to dashboard
+                    store.commit('loginUser');
+                    localStorage.setItem('token', response.data.access_token);
+                    this.$router.push({ name: 'dashboard' });
+                }).catch(error => {
+                    this.loginError = true;
+                });
             }
         }
     }
 </script>
 
 <style scoped>
-    .login-form {
-        width: 300px;
+    .form-wrapper {
+        min-height: 100%;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+    }
+    .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+    }
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
     }
 </style>
