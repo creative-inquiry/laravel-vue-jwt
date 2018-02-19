cea284129838a183b3357b73b22e6b14a6c80d48
Peter Plucinski <peter.plucinski@gmail.com>
2018-02-19 13:12:34 +1100

    Add a guard for authenticated routes, compile

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -6,27 +6,51 @@ Vue.use(VueRouter)
 import DashboardComponent from './components/DashboardComponent'
 import LoginComponent from './components/LoginComponent'
 import LogoutComponent from './components/LogoutComponent'
+import store from './store'
 
-export default new VueRouter({
-    routes: [
-        {
-            path: '/',
-            redirect: { name: 'login' }
-        },
-        {
-            path: '/dashboard',
-            name: 'dashboard',
-            component: DashboardComponent
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: LoginComponent
-        },
-        {
-            path: '/logout',
-            name: 'logout',
-            component: LogoutComponent
-        }
-    ]
+const routes = [
+    {
+        path: '/',
+        redirect: { name: 'login' }
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: DashboardComponent,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '/logout',
+        name: 'logout',
+        component: LogoutComponent
+    }
+];
+
+const router = new VueRouter({
+    routes
 });
+
+router.beforeEach((to, from, next) => {
+
+    // check if the route requires authentication and user is not logged in
+    if (to.matched.some(route => route.meta.requiresAuth) && !store.state.isLoggedIn) {
+        // redirect to login page
+        next({ name: 'login' })
+        return
+    }
+
+    // if logged in redirect to dashboard
+    if(to.path === '/login' && store.state.isLoggedIn) {
+        next({ name: 'dashboard' })
+        return
+    }
+
+    next()
+});
+
+export default router;
