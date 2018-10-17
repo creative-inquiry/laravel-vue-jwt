c0046ce0ee4d14f18aa7838306a2d39c928b5497
Peter Plucinski <peter.plucinski@gmail.com>
2018-10-17 17:44:19 +1100

    Remove redundant links to dashboard and login, compile

--- resources/assets/js/components/DashboardComponent.vue
+++ resources/assets/js/components/DashboardComponent.vue
@@ -5,8 +5,6 @@
                 <h1>Dashboard</h1>
 
                 <p>
-                    <router-link :to="{ name: 'dashboard' }">Dashboard</router-link> |
-                    <router-link :to="{ name: 'login' }">Login</router-link> |
                     <router-link :to="{ name: 'logout' }">Logout</router-link>
                 </p>
 
