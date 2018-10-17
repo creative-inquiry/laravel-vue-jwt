585ef290d4bc5e2bcc62a2a50e64748c90976c54
Peter Plucinski <peter.plucinski@gmail.com>
2018-10-17 17:41:51 +1100

    Add bootstrap branding image and run 'npm run production'

--- resources/assets/js/components/LoginComponent.vue
+++ resources/assets/js/components/LoginComponent.vue
@@ -2,7 +2,7 @@
     <div class="text-center form-wrapper">
 
         <form class="form-signin" v-on:submit.prevent="submitLogin">
-            <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+            <img class="mb-4" src="/img/bootstrap-solid.svg" alt="" width="72" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 
             <label for="inputEmail" class="sr-only">Email address</label>
