8706089f5d6083deb3cc187449242ec3573b956a
Peter Plucinski <peter.plucinski@gmail.com>
2018-10-18 13:16:33 +1100

    Check if stored token is still valid, if not logout user and redirect to login page

--- resources/assets/js/components/AppComponent.vue
+++ resources/assets/js/components/AppComponent.vue
@@ -3,5 +3,27 @@
 </template>
 
 <script>
-    export default {}
+    import store from '../store'
+    export default {
+        created() {
+
+            if(localStorage.token) {
+                axios.post('/api/auth/me', {
+                        headers: {Authorization: 'Bearer ' + localStorage.token}
+                    },
+                ).then(response => {
+                    store.commit('loginUser')
+                }).catch(error => {
+
+                    if (error.response.status === 401 || error.response.status === 403) {
+                        store.commit('logoutUser')
+                        localStorage.setItem('token', '')
+                        this.$router.push({name: 'login'})
+                    }
+
+                });
+            }
+
+        }
+    }
 </script>
